f19a642c64959619252b93312544194f82af59d6
Eunice <amakakra44@gmail.com>
2025-02-08 22:07:29 +0100

    updated

--- QifElse.py
+++ QifElse.py
@@ -1,9 +1,9 @@
 # Given an integer, , perform the following conditional actions:
 
 # If n is odd, print Weird
-# If n is even and in the inclusive range of  to , print Not Weird
-# If n is even and in the inclusive range of  to , print Weird
-# If n is even and greater than , print Not Weird
+# If n is even and in the inclusive range of 2 to 5 , print Not Weird
+# If n is even and in the inclusive range of 6 to 20 , print Weird
+# If n is even and greater than 20, print Not Weird
 # Input Format
 
 # A single line containing a positive integer, .
@@ -37,12 +37,12 @@
 # 24 and  is even, so it is not weird.
 
 def check_weird(n):
-    if n%2 == 1:
-     print("weird")
+    if n%2 == 1 or (n%2==0 and 6<=n<=20):
+     print("Weird")
     else:
-       print("not weird")
+       print("Not weird")
 
 # print("enter a number")
-n = int(input("enter a number"))
+n = int(input("Enter a number: ").strip())
 
 check_weird(n)
