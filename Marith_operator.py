ecf40208b21edce32ab7254d5cdd0f10e5161bde
Eunice <amakakra44@gmail.com>
2025-02-12 22:09:25 +0100

    updated

---
+++ Marith_operator.py
@@ -0,0 +1,24 @@
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+
+
+# if __name__ == '__main__':
+# if __name__ == '__main__':
+#     a = int(input())
+#     b = int(input())
+#     print(a+b)
+#     print(a-b)
+#     print(a*b)
+
+
+# -------------------Here-----------------------
+# another way to solve the above
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+print(a+b)
+print(a-b)
+print(a*b)
