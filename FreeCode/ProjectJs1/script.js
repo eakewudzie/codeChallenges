2dc2e78bc718d5450ebdbaa888749cb79e299ff7
Eunice <amakakra44@gmail.com>
2025-06-03 21:26:04 +0200

    for loop

--- FreeCode/ProjectJs1/script.js
+++ FreeCode/ProjectJs1/script.js
@@ -1,15 +1,26 @@
-let character = 'Hello';
-let count = 8;
-let rows = ["Naomi", "Quincy", "CamperChan"];
-let pushed = rows.push("freeCodeCamp");
-console.log(pushed);
-let popped = rows.pop();
-console.log(popped);
-console.log(rows);let character = 'Hello';
-let count = 8;
-let rows = ["Naomi", "Quincy", "CamperChan"];
-let pushed = rows.push("freeCodeCamp");
-console.log(pushed);
-let popped = rows.pop();
-console.log(popped);
-console.log(rows);
+
+
+
+// let character = 'Hello';
+// let count = 8;
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+// let pushed = rows.push("freeCodeCamp");
+// console.log(pushed);
+// let popped = rows.pop();
+// console.log(popped);
+// console.log(rows);
+
+
+
+
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+let result = ""
